7a1af5876cf74ce2515bd78fdb1a8cba88fb07b2
Nhatleprod <nhatle2611@gmail.com>
2025-05-17 13:20:07 +0700

    update models

--- models/exam_attempt_details.js
+++ models/exam_attempt_details.js
@@ -4,6 +4,7 @@ module.exports = function(sequelize, DataTypes) {
     id: {
       type: DataTypes.CHAR(36),
       allowNull: false,
+      defaultValue: Sequelize.Sequelize.fn('uuid'),
       primaryKey: true
     },
     examAttemptId: {
